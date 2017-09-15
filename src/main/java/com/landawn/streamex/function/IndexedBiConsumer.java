b0ed0fffb71f38f6099622396204c69e8d71186d
landawn <landawn@users.noreply.github.com>
2017-09-15 13:50:14 -0700

    1.0

---
+++ src/main/java/com/landawn/streamex/function/IndexedBiConsumer.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Haiyang Li
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.landawn.streamex.function;
+ 
+import java.util.Objects;
+
+/**
+ * 
+ * @author Haiyang Li
+ *
+ */
+public interface IndexedBiConsumer<U, T> {
+
+    void accept(U u, int idx, T e);
+
+    default IndexedBiConsumer<U, T> andThen(IndexedBiConsumer<? super U, ? super T> after) {
+        Objects.requireNonNull(after);
+
+        return (u, idx, e) -> {
+            accept(u, idx, e);
+            after.accept(u, idx, e);
+        };
+    }
+}
---
+++ src/main/java/com/landawn/streamex/function/IndexedFunction.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Haiyang Li
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.landawn.streamex.function;
+
+import java.util.Objects;
+
+/**
+ * @author Haiyang Li
+ *
+ */
+public interface IndexedFunction<T, R> {
+
+    R apply(int idx, T e);
+
+    default <V> IndexedFunction<V, R> compose(IndexedFunction<? super V, ? extends T> before) {
+        Objects.requireNonNull(before);
+
+        return (idx, v) -> apply(idx, before.apply(idx, v));
+    }
+
+    default <V> IndexedFunction<T, V> andThen(IndexedFunction<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+
+        return (idx, t) -> after.apply(idx, apply(idx, t));
+    }
+}
+
